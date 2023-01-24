7d1a8e95af97be32ee1bb14b1f216ab6952ca65a
Christian Ramirez <christian.r0602@gmail.com>
2023-01-23 20:01:34 -0600

    user object

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express')
 const app = express()
 const PORT = 8000;
+const { faker } =  require('@faker-js/faker');
+
+const userObj = {
+  password: faker.internet.password(),
+  email: faker.internet.email(),
+  phoneNumber: faker.phone.number(),
+  lastname: faker.name.lastName(),
+  firstName: faker.name.firstName(),
+  chemicalElement: faker.science.chemicalElement()
+}
+
+const companyObj = {
+  
+}
+
+
+const createUser = () => {
+  return userObj
+}
+
+console.log(createUser())
